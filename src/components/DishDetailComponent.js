3b6828090a230fb3854834d1ca2fd46ed2216c2c
Nina Boonyasiti <nina-boonyasiti@gmail.com>
2022-05-20 16:04:24 -0500

    component skeleton

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,37 +4,13 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 class DishDetail extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        }
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
+    render() {
+        return(
 
-    renderDish(dish) {
-        if(dish != null) {
-            return(
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>
-                     <CardBody>
-                         <CardTitle>{dish.name}</CardTitle>
-                         <CardText>{dish.description}</CardText>
-                    </CardBody> 
-                </Card>
-            );
-        }
-        else {
-            return(
-                <div></div>
-            );
-        }
+        );
     }
-
-    // return(
-    //     <div></div>
-    // );
 }
 
 export default DishDetail;
